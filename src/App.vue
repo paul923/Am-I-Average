4c06e42fd1843f7825f82f0e75d94755f7bce55a
Paul Kim <paulgmkim@gmail.com>
2019-06-21 12:04:56 -0700

    Files re-organized

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class='container'>
+    
+    <div id="init-screen" class="col-11 mx-auto h-75" style="display:">
+      <div class="text-center">
+        <p>Were you ever curious if you are an average person in certain situation?</p>
+        <p>
+          <span>Am I Average</span> will ask you interesting questions and provide results collected from honest and reliable users.
+        </p>
+        <p>So now...</p>
+        <p>Are You Average?</p>
+      </div>
+      <div class="text-center">
+        <button class="btn btn-primary">Let's get started!</button>
+      </div>
+    </div>
+
+    <div id="ui_question"></div>
+
+    <div id="ui_result" style="display:none"></div>
+    <div id="bl_question" style="display:none"></div>
+    <div id="bl_result" style="display:none"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import "bootstrap";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "animate.css/animate.css";
 
 Vue.config.productionTip = false
 
