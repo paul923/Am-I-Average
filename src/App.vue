8574bbd9a47189e3aeb04f051d4261d3495d5276
Paul Kim <paulgmkim@gmail.com>
2019-06-23 19:50:58 -0700

    initial screen completed

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,25 @@
 <template>
   <div id="app" class='container'>
     
-    <div id="init-screen" class="col-11 mx-auto h-75" style="display:">
-      <div class="text-center">
-        <p>Were you ever curious if you are an average person in certain situation?</p>
-        <p>
-          <span>Am I Average</span> will ask you interesting questions and provide results collected from honest and reliable users.
-        </p>
-        <p>So now...</p>
-        <p>Are You Average?</p>
-      </div>
-      <div class="text-center">
-        <button class="btn btn-primary">Let's get started!</button>
-      </div>
-    </div>
+    <init-screen></init-screen>
+
+
 
     <div id="ui_question"></div>
-
     <div id="ui_result" style="display:none"></div>
     <div id="bl_question" style="display:none"></div>
     <div id="bl_result" style="display:none"></div>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import InitScreen from './components/InitScreen.vue';
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    InitScreen
   }
 };
 </script>
---
+++ src/components/InitScreen.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    id="init-screen"
+    class="col-11 mx-auto h-75 border border-primary p-4 rounded"
+    v-if="!isHidden">
+
+    <div id="messages" class="text-center">
+      <p>Were you ever curious if you are an average person in certain situation?</p>
+      <p>Am I Average will ask you interesting questions and provide results collected from honest and reliable users.</p>
+      <p>So now...</p>
+      <p>Are You Average?</p>
+    </div>
+    
+    <div class="text-center">
+      <button 
+      class="btn btn-primary"
+      v-on:click = "isHidden = true">Let's get started</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      isHidden : false
+    }
+  }
+};
+</script>
