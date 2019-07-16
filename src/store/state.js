2e4722ee7564664dee6580ae06245ca3326f5e24
Paul Kim <paulgmkim@gmail.com>
2019-07-15 18:26:09 -0700

    api key hidden

--- src/store/state.js
+++ src/store/state.js
@@ -2,20 +2,10 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import { firebase } from "@firebase/app";
 import "@firebase/firestore";
-// import { db } from "../firebase";
+import { config } from "../firebase"
 
 Vue.use(Vuex);
 
-//Firebase init
-const config = {
-  apiKey: "AIzaSyAg7M_-b7epJVuMoBcvlUTWct7gFnVIOco",
-  authDomain: "am-i-average.firebaseapp.com",
-  databaseURL: "https://am-i-average.firebaseio.com",
-  projectId: "am-i-average",
-  storageBucket: "am-i-average.appspot.com",
-  messagingSenderId: "247200672415",
-  appId: "1:247200672415:web:75094b7bd1722d82"
-}
 firebase.initializeApp(config)
 export const db = firebase.firestore()
 
